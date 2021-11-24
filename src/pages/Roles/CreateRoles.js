a7c8bccb8d961ff69894cf1113c3a87b0f250363
sureshkanhasoft <suresh.kanhasoft@gmail.com>
2021-11-24 15:03:20 +0530

    roles page changes

--- src/pages/Roles/CreateRoles.js
+++ src/pages/Roles/CreateRoles.js
@@ -22,27 +22,43 @@ const CreateRoles = ({ open, handleClose }) => {
     const dispatch = useDispatch();
     const [roleMsg, setRoleMsg] = useState(false)
     const { roleErrors, roleSuccess } = useSelector(state => state.roles)
-    const { register, handleSubmit, formState: { errors }, reset } = useForm();
+    // const { register, handleSubmit, formState: { errors }, reset } = useForm();
     const [data, setData] = useState({
         role_name: "",
     })
+    const [formError, setError] = useState([])
 
     const handleChange = (event) => {
+        if(event.target.value){
+            setError([]);
+        }else{
+            setError('err_role_name');
+        }
         setData({ ...data, [event.target.name]: event.target.value });
     };
 
-    const onSubmitRoles = async datas => {
-        dispatch(createRoles(datas))
-        setRoleMsg(true)
-        handleClose();
-        reset();
-        setData("")
+    const onSubmitRoles = async (event) => {
+        event.preventDefault();
+        if(data.role_name === ""){
+            setError('err_role_name');
+        }else{
+            dispatch(createRoles(data))
+            setRoleMsg(true)
+            handleClose();
+            setData("")
+        }
+       
 
     };
     const closeModal = () => {
-        reset()
+        resetForm()
         setData("")
         handleClose();
+    }
+
+    const resetForm = () =>{
+        setError([])
+        setData({ ...data, role_name: '' });
     }
     return (
         <>
@@ -58,8 +74,8 @@ const CreateRoles = ({ open, handleClose }) => {
                     status="success"
                 />
             }
-            <Dialog open={open} onClose={handleClose} classes={{ paper: classes.dialogWidth }}>
-                <form onSubmit={handleSubmit(onSubmitRoles)}>
+            <Dialog open={open} onClose={closeModal} classes={{ paper: classes.dialogWidth }}>
+                <form onSubmit={(event) => onSubmitRoles(event)}>
                     <DialogTitle id="form-dialog-title">
                         <div>Create Role</div>
                     </DialogTitle>
@@ -75,10 +91,12 @@ const CreateRoles = ({ open, handleClose }) => {
                                 name="role_name"
                                 value={data.role_name || ""}
                                 fullWidth
-                                {...register("role_name", {
-                                    required: "Please enter role name",
-                                })}
-                                error={(errors.role_name ? true : false)}
+                                // {...register("role_name", {
+                                //     required: "Please enter role name",
+                                // })}
+                                // error={(errors.role_name ? true : false)}
+                                error={(formError.includes('err_role_name') ? true : false)}
+                                helperText={(formError.includes('err_role_name')) ? "Please enter role name" : false}
                                 onChange={handleChange}
                             />
                         </div>
