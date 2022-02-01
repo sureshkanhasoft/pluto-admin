46a670d02dfb0d9a56aa32165e49e60815048729
ketan <ketan.kanhasoft@gmail.com>
2022-02-01 17:54:13 +0530

    change condition

--- src/pages/Bookings/CandidatesList.js
+++ src/pages/Bookings/CandidatesList.js
@@ -271,7 +271,7 @@ const CandidatesList = ({ bookingDetail, booking_id, getBookingDetail, setConfir
 
     return (
         <>
-            {confirmBookingError &&
+            {confirmBookingError && confirmBookingError?.message &&
                 <Notification
                     data={confirmBookingError.message}
                     status="error"
